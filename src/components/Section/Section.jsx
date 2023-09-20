8222280cb18a839015ac2eacde1a40b18e80488d
Orelba <orel450@gmail.com>
2023-09-20 20:35:20 +0300

    Add classname prop to Section Component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,10 @@
 import styles from './section.module.css'
 import cx from 'classnames'
 
-export default function Section({ padding, children }) {
+export default function Section({ padding, className, children }) {
   return (
     <div
-      className={cx(styles.section, {
+      className={cx(styles.section, className, {
         [styles['pad-both']]: padding === 'both',
         [styles['pad-bottom']]: padding === 'bottom',
         [styles['pad-top']]: padding === 'top',
